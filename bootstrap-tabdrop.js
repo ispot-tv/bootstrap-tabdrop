f30ad8ed5dbfc5ac8648e72b4a7fa471fdc5f4fd
Eliot Carlsen <eliot.carlsen@gmail.com>
2018-02-15 08:55:47 -0800

    Update bootstrap-tabdrop.js

--- bootstrap-tabdrop.js
+++ bootstrap-tabdrop.js
@@ -3,7 +3,20 @@
  * Adam Youngers
  * http://adamyoungers.com
  * Based on the work of Stefan Petre, http://www.eyecon.ro/bootstrap-tabdrop
+ * =========================================================
  * Copyright 2014 Adam Youngers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * ========================================================= */
 
 !function (e) {
@@ -19,11 +32,11 @@
                 e(t + " .li-more .dropdown-menu").prepend(n),
                 e(t + " > li:nth-last-child(2)").remove(),
                 e(t + " .li-more .dropdown-menu .divider").remove(),
-                e(t + " .li-more .dropdown-menu *").removeAttr("role data-toggle aria-expanded").removeClass("dropdown-menu dropdown-toggle dropdown caret js-subnav-params-ignore"),
+                e(t + " .li-more .dropdown-menu *").removeAttr("role data-toggle aria-expanded").removeClass("dropdown-menu dropdown-toggle dropdown caret fa fa-angle-down nav-tabs__caret js-subnav-params-ignore"),
                 o();
             }
         }
-        var t = this.selector, r = e(t).html(), i = e(t).outerWidth(!0), a = o(), d = '<li class="li-more dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">more <span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></li>';
+        var t = '.' + this[0].className.replace(/\s/gi, '.'), r = e(t).html(), i = e(t).outerWidth(!0), a = o(), d = '<li class="li-more dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false"><span class="nav-tabs__title">More <span class="nav-tabs__title-second">Insights</span></span><i class="fa fa-angle-down nav-tabs__caret"></i></a><ul class="dropdown-menu" role="menu"></ul></li>';
         return n(), setTimeout(function () {
             e(window).on("resize", function () {
                 setTimeout(function () {
@@ -33,3 +46,4 @@
         }, 500), this
     }
 }(jQuery);
+
